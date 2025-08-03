c1f2ff11ede2ca93160dd1aa89d92935c4ca0fc1
diego-ramos98 <diego.ramos.fortunato@outlook.com>
2025-08-03 08:43:12 -0300

    :feat:(service) cria classe service

--- src/main/java/com/diego/condominio/service/UsuarioService.java
+++ src/main/java/com/diego/condominio/service/UsuarioService.java
@@ -1,8 +1,12 @@
 package com.diego.condominio.service;
 
+import com.diego.condominio.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
 
 }
